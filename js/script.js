6e47ef7925aef09db153a42f94b99ac0eca3e1e9
Chagorovski <52652042+Chagorovski@users.noreply.github.com>
2021-03-06 14:26:34 +0100

    Fetch external quotes API

--- js/script.js
+++ js/script.js
@@ -1,51 +1,11 @@
 // An array with random quotes 
-
-let quotes = [
-  {
-    quote : "Deep in the human unconscious is a pervasive need for a logical universe that makes sense. But the real universe is always one step beyond logic",
-    source : "Frank Herbert",
-    citation : "Facebook",
-    tag : "#motivated"
-  },
-  {
-    quote : "I loved her against reason, against promise, against peace, against hope, against happiness, against all discouragement that could be.",
-    source : "Charles Dickens",
-    citation : "Great Expectations",
-    tag : "#love",
-    year : 1861
-  },
-  {
-    quote : "Think before you speak. Read before you think",
-    source : "Fran Lebowitz",
-    citation : "Facebook",
-    tag : "#smart"
-  },
-  {
-    quote : "Time is the longest distance between two places.",
-    source : "Tennessee Williams",
-    citation : "The Glass Menagerie",
-    year : 1944
-  },
-  {
-    quote : "The greatest victory in life is to rise above the material things that we once valued most.",
-    source : " Muhammad Ali and Hana Yasmeen Ali",
-    citation : "The Soul of a Butterfly: Reflections on Life's Journey",
-    tag: "#materialsim"
-  },
-  {
-    quote : "But there is only one thing that has power completely, and this is love.",
-    source : "Alan Paton",
-    citation : "Cry, the Beloved Country",
-    year : 1948
-  },
-  {
-    quote : "Change before you have to.",
-    source : " Jack Welch",
-    citation : "Winning",
-    tag : "#one Perfect Sentence"
-  }
-];
-
+let quotes = [];
+let quotesUrl = fetch("https://type.fit/api/quotes")
+  .then(response => response.json())
+  .then(data => data.forEach(element => {
+    quotes.push(element);
+  }))
+  .catch(error => console.log('error is', error));
 
 // This function generates a random quote
 function getRandomQuote () {
@@ -75,8 +35,8 @@ function printQuote () {
   let callGetRandomQuote = getRandomQuote();
   let htmlString = '';
   let createSpan = document.createElement('span')
-  htmlString += '<p class="quote">' + callGetRandomQuote.quote + '</p>';
-  htmlString += '<p class="source">' + callGetRandomQuote.source;
+  htmlString += '<p class="quote">' + callGetRandomQuote.text + '</p>';
+  htmlString += '<p class="source">' + callGetRandomQuote.author;
 
 // Statement to check for the citation property before adding it
   if (callGetRandomQuote.citation) {
